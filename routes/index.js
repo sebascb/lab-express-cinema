717454281fdd059f04fb306276b94660a18c6916
Sebas Corral <sebascorralbrito@gmail.com>
2021-12-10 22:25:31 +0100

    part done

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const Movie = require("../model/Movie.model.js");
+
 
 /* GET home page */
-router.get('/', (req, res, next) => res.render('index'));
+router.get("/", (req, res, next) => res.render("index"));
+
+router.get("/movies", (req, res) => {
+    Movie.find()
+    .then((movies) => {
+        res.render("movies", {movies});
+    });
+});
+
+router.get("/movies/:id", (req, res) => {
+  const {id} = req.params;
+
+  Movie.findById(id)
+    .then((movie) => {
+        res.render('moviedetails', {movie})
+    })   
+});
 
 module.exports = router;
